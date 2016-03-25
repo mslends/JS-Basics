3379cea4e7eafacf53a19a8582c537c5d156fae0
Mike Spencer <mslends@gmail.com>
2016-03-25 16:53:26 -0600

    Bout to restart dis.

--- closures.js
+++ closures.js
@@ -1,26 +1,16 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
-
 var outer = function(){
   var name = 'Tyler';
   return function(){
     return 'The original name was ' + name;
   }
-}
-//Above you're given a function that returns another function which has a closure over the name variable.
-//Invoke outer saving the return value into another variable called 'inner'.
-
+};
+// Above you're given a function that returns another function which has a closure over the name variable.
+// Invoke outer saving the return value into another variable called 'inner'.
   //Code Here
-var inner = outer();
 //Once you do that, invoke inner.
-
   //Code Here
-inner();
-
-
 //Next problem
-
-
-
 var callFriend = function(){
   var friend = 'Jake';
   function callF(number){
@@ -28,143 +18,75 @@ var callFriend = function(){
   }
   return callF;
 };
-
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
-
   //Code Here
-var callJake = callFriend()
-console.log(callJake ("435-215-9248"))
-
-
 //Next Problem
-
-
-
 /*
   Write a function called makeCounter that makes the following code work properly.
-*/
-
-  //Code Here
-  function makeCounter() {
-    var start = 0;
-    return function() {
-      start++;
-      return start;
-    }
-  };
-  var count = makeCounter();
-  count(); // 1
-  count(); // 2
-  count(); // 3
-  count(); // 4
+*/  //Code Here
 
 
-
+//Uncomment this once you make your function
+//   var count = makeCounter();
+//   count(); // 1
+//   count(); // 2
+//   count(); // 3
+//   count(); // 4
 //Next Problem
-
-
-
-/*
-  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
-*/
-
-  //Code Here
-function codeLove () {
-  return "I love code";
-
+// Inside the function called counterFactory
+// return two functions that implement up/down counter.
+// The first function is called inc, this function is responsible for incrementing the value once
+// The second function is called dec, this function is responsible for decrementing the value by one
+// You will need to use the module pattern to achieve this.
+function counterFactory(value) {
+  return {}
+    // Code inc function
+    // Code dec function
 }
-function codeFriend(fn) {
-  var limiter = 0;
-  return function() {
-    if(limiter < 1) {
-      limiter++;
-      return fn();
-    }else {
-      return null;
-    }
-
+counter = counterFactory(10);
+//Next Problem
+// Inside the motivation function create another function called message that will return welcomeText + firstname + lastname
+  function motivation(firstname, lastname){
+    var welcomeText = 'Your doing awesome keep it up    ';
+    // code message function here
+    //Uncommment this to return the value of your invoked message function
+    // return message()
   }
-
-}
-var codeEcho = codeFriend(codeLove);
-
-
-//Next Problem
-
-
-
-/*
-  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
-*/
-function fnCounter(fn, N) {
-  var number = N;
-  return function() {
-    if(number <= N) {
-      return fn();
-      number++;
-    }else {
-      return "STOP";
-    }
-
-  }
-
-
-}
-
-
-//Next Problem
-
-
-
-/*
-  var counter = function(){
-    for (var i=1; i<=5; i++) {
-      setTimeout( function timer(){
-          console.log( i );
-      }, i*1000 );
-    }
-  };
-
-  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
-
-    //Answer Here
-// i will pop up after 1 second.
-
-  Now, run the function in your console and note what happpens.
-
-  Was your answer right or wrong?
-
-    //Answer Here
-
-wrong
-  Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one because of the timeout)
-*/
-
-    //Code Here
-    var counter = function(){
-      for (var i=1; i<=5; i++) {
-        setTimeout( function timer(param1){
-            console.log( i );
-        }, i*1000 );
-      }
+  motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
+// Inside the return create a publicMethod property that is a function that invokes privateMethod. After you create the privateMethod
+// Invoke it by calling module.publicMethod(); outside the module scope
+  var module = (function() {
+    var person = {
+      name: "phillip",
+      age: 29,
+      location: 'Utah'
     };
-    counter();
-
-
-//Next Problem
-
-
-
+    var privateMethod = function(){
+      return welcomeText + firstname + '  ' + lastname;
+    };
+    // Anything that is being returned is made public and can be invoked from outside our lexical scope
+    return {
+      // Code here
+    };
+  })();
+//Uncomment this after you create your public method
+//   module.publicMethod();
+// Here we have a for loop that will iterate as long as i is less than or equal to 5. What we need to do is console.log(i)
+// So that it logs ( 1 then 2 then 3, etc). Run this code in your console to see what the output is.
+  for (var i = 0; i <= 5; i++) {
+    setTimeout(function() {
+      console.log(i)
+    }, i * 1000)
+  }
+  // To make this code work you will need to create a new scope for every iteration.
 /*
   Make the following code work
-
   funcArray[0]() //0
   funcArray[1]() //1
   funcArray[2]() //2
   funcArray[3]() //3
   funcArray[4]() //4
   funcArray[5]() //5
-
   *Hint: Don't let this fool you. Break down what's really happening here.
 */
